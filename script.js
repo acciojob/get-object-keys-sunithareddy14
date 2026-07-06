3c4ea41e0cc2d8d91af9f5069ddab365fa66ee08
sunithareddy14 <sunithareddy.s1437@gmail.com>
2026-07-06 20:04:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //your JS code here. If required.
-const student{
+const student = {
 	name : "sunitha",
-}
+};
 function getKeys(obj){
 	return Object.keys(obj);
 }
+console.log(getKeys(student));
+
+
